948d9ebaf147439322b32caa7f4433ed9a492f62
Jenly <jenly1314@gmail.com>
2020-04-09 19:23:44 +0800

    Update versions

--- app/src/androidTest/java/com/king/template/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/king/template/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package com.king.template
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 import org.junit.Test
 import org.junit.runner.RunWith
